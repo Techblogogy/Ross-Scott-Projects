34fc205dc012fed273c29881fd8ebe95885434d0
Techblogogy <fedorbobylev@gmail.com>
2014-06-12 11:42:04 +0300

    ScreenMagnifier V1.0 Release
    The Best version ever.

--- ScreenMag/src/ScreenMag.java
+++ ScreenMag/src/ScreenMag.java
@@ -10,13 +10,30 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.util.Arrays;
+
 import com.sun.jna.*;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.win32.*;
+import com.sun.jna.Memory;
+import com.sun.jna.platform.win32.GDI32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HBITMAP;
+import com.sun.jna.platform.win32.WinDef.HDC;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinDef.INT_PTR;
+import com.sun.jna.platform.win32.WinDef.RECT;
+import com.sun.jna.platform.win32.WinGDI;
+import com.sun.jna.platform.win32.WinGDI.BITMAPINFO;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+
+import jna.extra.GDI32Extra;
+import jna.extra.User32Extra;
+import jna.extra.WinGDIExtra;
 
 public class ScreenMag {
 	public static Robot rob;
@@ -36,53 +53,51 @@ public class ScreenMag {
 	public static int topB = 30;
 	public static int sideB = 8;
 	
+	public static HWND hWnd;
+	
+	public static int screenId = 0;
+	
 	ScreenMag()
 	{
 		
 	}
 	
-	public interface User32 extends StdCallLibrary 
+	public static BufferedImage capture(HWND hWnd)
 	{
-		User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
-		HWND FindWindow(String lpClassName, String lpWindowName);
-		
-		int GetWindowRect(HWND handle, int[] rect);
-	}
-	
-	public static int[] getRect(String windowName) throws WindowNotFoundException, GetWindowRectException 
-	{
-		HWND hwnd = User32.INSTANCE.FindWindow(null, windowName);
-		
-		if (hwnd == null) 
-		{
-			throw new WindowNotFoundException("", windowName);
-		}
-		
-		int[] rect = {0, 0, 0, 0};
-		int result = User32.INSTANCE.GetWindowRect(hwnd, rect);
-		
-		if (result == 0)
-		{
-			throw new GetWindowRectException(windowName);
-		}
-		
-		return rect;
-	}
-	
-	@SuppressWarnings("serial")
-	public static class WindowNotFoundException extends Exception 
-	{
-		public WindowNotFoundException(String className, String windowName) 
-		{
-			super(String.format("Window null for className: %s; windowName: %s", className, windowName));
-		}
-	}
-	
-	@SuppressWarnings("serial")
-	public static class GetWindowRectException extends Exception {
-		public GetWindowRectException(String windowName) {
-			super("Window Rect not found for " + windowName);
-		}
+		HDC hdcWindow = User32.INSTANCE.GetDC(hWnd);
+        HDC hdcMemDC = GDI32.INSTANCE.CreateCompatibleDC(hdcWindow);
+
+        RECT bounds = new RECT();
+        User32Extra.INSTANCE.GetClientRect(hWnd, bounds);
+
+        wWidth = bounds.right - bounds.left;
+        wHeight = bounds.bottom - bounds.top;
+
+        HBITMAP hBitmap = GDI32.INSTANCE.CreateCompatibleBitmap(hdcWindow, wWidth, wHeight);
+
+        HANDLE hOld = GDI32.INSTANCE.SelectObject(hdcMemDC, hBitmap);
+        GDI32Extra.INSTANCE.BitBlt(hdcMemDC, 0, 0, wWidth, wHeight, hdcWindow, 0, 0, WinGDIExtra.SRCCOPY);
+
+        GDI32.INSTANCE.SelectObject(hdcMemDC, hOld);
+        GDI32.INSTANCE.DeleteDC(hdcMemDC);
+
+        BITMAPINFO bmi = new BITMAPINFO();
+        bmi.bmiHeader.biWidth = wWidth;
+        bmi.bmiHeader.biHeight = -wHeight;
+        bmi.bmiHeader.biPlanes = 1;
+        bmi.bmiHeader.biBitCount = 32;
+        bmi.bmiHeader.biCompression = WinGDI.BI_RGB;
+
+        Memory buffer = new Memory(wWidth * wHeight * 4);
+        GDI32.INSTANCE.GetDIBits(hdcWindow, hBitmap, 0, wHeight, buffer, bmi, WinGDI.DIB_RGB_COLORS);
+
+        BufferedImage image = new BufferedImage(wWidth, wHeight, BufferedImage.TYPE_INT_RGB);
+        image.setRGB(0, 0, wWidth, wHeight, buffer.getIntArray(0, wWidth * wHeight), 0, wWidth);
+
+        GDI32.INSTANCE.DeleteObject(hBitmap);
+        User32.INSTANCE.ReleaseDC(hWnd, hdcWindow);
+
+        return image;
 	}
 	
 	public static void main(String[] args) 
@@ -102,35 +117,41 @@ public class ScreenMag {
 		sWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
 		sHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
 		
-		/*GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		GraphicsDevice gd = ge.getDefaultScreenDevice();
-		GraphicsConfiguration gc = gd.getDefaultConfiguration();
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gd = ge.getScreenDevices();
 		
-		gd.setFullScreenWindow(win);*/
+		if (args[1] != null)
+		{		
+			screenId = Integer.parseInt(args[1])-1;
+			System.out.println(screenId);
+			
+			sWidth = gd[screenId].getDisplayMode().getWidth();
+			sHeight = gd[screenId].getDisplayMode().getHeight();
+			
+			System.out.println(sWidth);
+			System.out.println(sHeight);
+			
+			win.setLocation( Integer.parseInt(args[2]), Integer.parseInt(args[3]) );
+		}
+		
+		//GraphicsConfiguration gc = gd.getDefaultConfiguration();
+		
+		//gd.setFullScreenWindow(win);
 		
 		String windowName = args[0]; //"Grim Fandango";
 		int[] rect;
+		nWidth = (float)wWidth/(float)wHeight * sHeight;
+		nX = (float)sWidth/(float)2 - nWidth/(float)2;
 		
-		try 
+		hWnd = User32.INSTANCE.FindWindow(null, windowName);
+		if (hWnd == null)
 		{
-			rect = getRect(windowName);
-			
-			wX = rect[0]+sideB; //1928;
-			wY = rect[1]+topB; //30;
-			wWidth = (rect[2] - wX)-sideB; //640;
-			wHeight = (rect[3] - wY)-sideB; //478;
-			
-			//System.out.println(wX+" "+wY+" "+(wWidth-wX+" "+(wHeight-wY));
-		} catch (WindowNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (GetWindowRectException e) {
-			e.printStackTrace();
 			System.exit(1);
 		}
 		
-		nWidth = (float)wWidth/(float)wHeight * sHeight;
-		nX = (float)sWidth/(float)2 - nWidth/(float)2;
+		/*User32.INSTANCE.MoveWindow(hWnd, 10, 10, 100, 100, true);
+		
+		System.exit(1);*/
 		
 		//Create Canvas
 		Canvas cnv = new Canvas();
@@ -162,26 +183,14 @@ public class ScreenMag {
 				graphics.setColor(Color.BLACK);
 				graphics.fillRect(0, 0, sWidth, sHeight);
 				
-				try 
-				{
-					rect = getRect(windowName);
-					
-					wX = rect[0]+sideB; //1928;
-					wY = rect[1]+topB; //30;
-					wWidth = (rect[2] - wX)-sideB; //640;
-					wHeight = (rect[3] - wY)-sideB; //478;
-					
-					//System.out.println(wX+" "+wY+" "+(wWidth-wX+" "+(wHeight-wY));
-				} catch (WindowNotFoundException e) {
-					e.printStackTrace();
-					System.exit(1);
-				} catch (GetWindowRectException e) {
-					e.printStackTrace();
-					System.exit(1);
-				}
+				hWnd = User32.INSTANCE.FindWindow(null, windowName);
+				if (hWnd == null)
+					System.exit(0);
 				
-				//TODO DrawStuff
-				dImg = rob.createScreenCapture(new Rectangle(wX,wY,wWidth,wHeight));
+				dImg = capture(hWnd);
+				
+				nWidth = (float)wWidth/(float)wHeight * sHeight;
+				nX = (float)sWidth/(float)2 - nWidth/(float)2;
 				
 				graphics.drawImage(dImg, (int)nX, 0, (int)nWidth, sHeight,null);
 				
